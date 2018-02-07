045685913a41e899c286f76ec1aefd355bd50de2
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-02-07 00:15:01 -0500

    SearchDatabase.java compiles now :)

--- SearchDatabase.java
+++ SearchDatabase.java
@@ -2,6 +2,7 @@ package search;
 
 import java.util.*;
 import java.nio.file.Paths;
+import java.io.IOException;
 // lucene imports
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -15,16 +16,20 @@ import org.apache.lucene.store.*;
 public class SearchDatabase {
 	private SearchDatabase() {}
 
-	public static void main(String[] args) {
-		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get('index/')));
+	public static void main(String[] args) throws IOException {
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get("index/")));
     IndexSearcher searcher = new IndexSearcher(reader);
     Analyzer analyzer = new StandardAnalyzer();
     QueryParser parser = new QueryParser("description", analyzer);
-    Query query = parser.parse("oberlin");
-    TopDocs results = searcher.search(query, 5);
-    ScoreDoc[] hits = results.scoreDocs;
-    for (int i = 0; i < 5; i++) {
-    	System.out.println(hits[i]);
+    try {
+    	Query query = parser.parse("oberlin");
+    	TopDocs results = searcher.search(query, 5);
+	    ScoreDoc[] hits = results.scoreDocs;
+	    for (int i = 0; i < 5; i++) {
+	    	System.out.println(hits[i]);
+	    }
+    } catch (Exception e) {
+    	//
     }
 	}
 }
